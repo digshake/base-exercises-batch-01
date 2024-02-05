9ae5092237e2154a5564281534145e9f1b0a86f8
dssho <dssho@DESKTOP-2ASM3QT.dhcp.wustl.edu>
2024-02-05 12:18:46 -0600

    Updating 2D array example

--- src/module3/_5traversing2d/Average.java
+++ src/module3/_5traversing2d/Average.java
@@ -2,20 +2,6 @@ package module3._5traversing2d;
 
 public class Average
 {
-
-   public static double getAvg(int[][] a)
-   {
-      double total = 0;
-      for (int[] innerArray : a)
-      {
-         for (int val : innerArray)
-         {
-            total = total + val;
-         }
-      }
-      return total / (a.length * a[0].length);
-   }
-
    public static void main(String[] args)
    {
       int[][] a = {  {80, 90, 70}, {20, 80, 75}};
